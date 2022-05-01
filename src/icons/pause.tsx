645340900ff52c8eb068e6f72ecf67bc2f36a718
007 <dr007cc@gmail.com>
2022-05-01 17:02:28 +0430

    adding play & pause icons

---
+++ src/icons/pause.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+            <path
+                d='M 208 432 H 160 A 16 16 0 0 1 144 416 V 96 A 16 
+                   16 0 0 1 160 80 H 208 A 16 16 0 0 1 224 96 V 416 
+                   A 16 16 0 0 1 208 432 Z M 352 432 H 304 A 16 16 
+                   0 0 1 288 416 V 96 A 16 16 0 0 1 304 80 H 352 A 
+                   16 16 0 0 1 368 96 V 416 A 16 16 0 0 1 352 432 Z'
+            />
+        </svg>
+    )
+}
