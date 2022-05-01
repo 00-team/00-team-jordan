09e6b70103fa3c90cb236f5c7131e4d3342b3b03
007 <dr007cc@gmail.com>
2022-05-01 22:11:00 +0430

    updating the demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -5,13 +5,18 @@ import { createRoot } from 'react-dom/client'
 import './style.scss'
 
 // player
-import { Player, Source } from '..'
+import { Player, Source } from '../lib'
 
 import green from './videos/green.mp4'
 import red from './videos/red.mp4'
 import blue from './videos/blue.mp4'
 
-const source: Source = [green, red, blue]
+const source: Source = [
+    // 'https://cdn.discordapp.com/attachments/876184324133752874/944696426544373790/nima_1384_bzan_20220219_222030_0.mp4',
+    green,
+    red,
+    blue,
+]
 
 const App: FC = () => {
     return (
