04f13cd3a4ecd3780200699fcf643a89f6059154
007 <dr007cc@gmail.com>
2022-05-02 21:05:41 +0430

    updating demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -12,6 +12,7 @@ import red from './videos/red.mp4'
 import blue from './videos/blue.mp4'
 
 const source: Source = [
+    // 'https://cdn.discordapp.com/attachments/837976157609656323/842785794485780520/Shrek_1.mp4',
     // 'https://cdn.discordapp.com/attachments/876184324133752874/944696426544373790/nima_1384_bzan_20220219_222030_0.mp4',
     green,
     red,
@@ -21,7 +22,9 @@ const source: Source = [
 const App: FC = () => {
     return (
         <div className='app'>
-            <Player source={source} />
+            <div className='app-video'>
+                <Player source={source} />
+            </div>
         </div>
     )
 }
