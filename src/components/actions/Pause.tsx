59ee3442e82ee14d17943d107bb776e6d6e2fe42
007 <dr007cc@gmail.com>
2022-05-02 21:05:04 +0430

    adding pause action

---
+++ src/components/actions/Pause.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react'
+import BaseComponent from 'BaseComponent'
+
+// icons
+import Icon from 'icons/pause'
+
+interface PauseProps {}
+
+interface PauseState {}
+
+class Pause extends BaseComponent<PauseProps, PauseState> {
+    override state: PauseState = {}
+
+    private pause() {
+        if (!this.video.paused) this.video.pause()
+    }
+
+    override render(): ReactElement {
+        return (
+            <button onClick={() => this.pause()}>
+                <Icon />
+            </button>
+        )
+    }
+}
+
+export { Pause }
+export default Pause
