d8c1badf37d7bb4288fe208907a71221163c0a8d
007 <dr007cc@gmail.com>
2022-05-04 14:17:48 +0430

    adding timeline functionalities

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,31 +1,45 @@
 import React, { ReactElement } from 'react'
+import { MouseEvent as RMouseEvent, TouchEvent as RTouchEvent } from 'react'
 import BaseComponent from 'BaseComponent'
 
 const F = (n: number) => Math.floor(n)
 
 type TP = (container: number, number: number) => number
 const P: TP = (c, n) => (100 / c) * n
-const PP: TP = (...args) => {
-    let p = P(...args)
-    if (p > 100) return 100
-    if (p < 0) return 0
 
-    return p
-}
-PP
 F
+
+// type timeline element
+type TTLEL = HTMLDivElement
 
 interface TimelineProps {}
 
 interface TimelineState {
     percentage: number
     onHold: boolean
+    node?: TTLEL
 }
 
 class Timeline extends BaseComponent<TimelineProps, TimelineState> {
     override state: TimelineState = {
         percentage: 0,
         onHold: false,
+    }
+
+    private UpdatePercentage(clientX: number) {
+        if (!this.state.node) return
+
+        const { left, width } = this.state.node.getBoundingClientRect()
+        let percentage = ((clientX - left) / width) * 100
+        if (percentage > 100) percentage = 100
+        if (percentage < 0) percentage = 0
+
+        let time = (this.video.duration / 100) * percentage
+
+        this.setState({ percentage: percentage })
+        if (!isNaN(time)) this.video.currentTime = time
+
+        // this.video.dispatchEvent(new Event('timeupdate'))
     }
 
     private UpdateTime = this.UpdateTimePR.bind(this)
@@ -35,24 +49,70 @@ class Timeline extends BaseComponent<TimelineProps, TimelineState> {
         })
     }
 
+    private MouseDown = this.MouseDownPR.bind(this)
+    private MouseDownPR(e: RMouseEvent<TTLEL, MouseEvent>) {
+        this.UpdatePercentage(e.clientX)
+        this.setState({ onHold: true })
+
+        document.addEventListener('mousemove', this.MouseMove)
+        document.addEventListener('mouseup', this.MouseUp)
+    }
+
+    private MouseMove = this.MouseMovePR.bind(this)
+    private MouseMovePR(e: MouseEvent) {
+        e.preventDefault()
+        this.UpdatePercentage(e.clientX)
+    }
+
+    private MouseUp = this.MouseUpPR.bind(this)
+    private MouseUpPR() {
+        document.removeEventListener('mousemove', this.MouseMove)
+        document.removeEventListener('mouseup', this.MouseUp)
+        this.setState({ onHold: false })
+    }
+
+    private TouchStart = this.TouchStartPR.bind(this)
+    private TouchStartPR(e: RTouchEvent<TTLEL>) {
+        if (e.touches.length <= 0 || !e.touches[0]) return
+
+        this.setState({ onHold: true })
+        this.UpdatePercentage(e.touches[0].clientX)
+
+        document.addEventListener('touchmove', this.TouchMove)
+        document.addEventListener('touchend', this.TouchEnd)
+    }
+
+    private TouchMove = this.TouchMovePR.bind(this)
+    private TouchMovePR(e: TouchEvent) {
+        if (e.touches.length <= 0 || !e.touches[0]) return
+        this.UpdatePercentage(e.touches[0].clientX)
+    }
+
+    private TouchEnd = this.TouchEndPR.bind(this)
+    private TouchEndPR() {
+        document.removeEventListener('touchmove', this.TouchMove)
+        document.removeEventListener('touchend', this.TouchEnd)
+        this.setState({ onHold: false })
+    }
+
     override componentDidMount() {
         // time
         this.video.addEventListener('timeupdate', this.UpdateTime)
     }
 
     override componentWillUnmount() {
-        // time
         this.video.removeEventListener('timeupdate', this.UpdateTime)
+        document.removeEventListener('mousemove', this.MouseMove)
+        document.removeEventListener('mouseup', this.MouseUp)
     }
 
     override render(): ReactElement {
         return (
             <div
                 className='timeline-container'
-
-                // onMouseDown={this.HandleMouseDownBind}
-                //     onTouchStart={this.HandleTouchStartBind}
-                //     ref={this.TimeLineRef.bind(this)}
+                onMouseDown={this.MouseDown}
+                onTouchStart={this.TouchStart}
+                ref={node => node && this.setState({ node: node })}
             >
                 <div className='timeline'>
                     <div
