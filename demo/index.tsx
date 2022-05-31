a725fddc903ce2db0c5b4dab95b096c7bed90385
007 <dr007cc@gmail.com>
2022-05-31 23:48:45 +0430

    updating demo videos

--- demo/index.tsx
+++ demo/index.tsx
@@ -7,27 +7,34 @@ import './style.scss'
 // player
 import { Player, Source } from '../lib/'
 
-import green from './videos/green.mp4'
-import red from './videos/red.mp4'
-import blue from './videos/blue.mp4'
+import duck_144p from './videos/144p.mp4'
+import duck_240p from './videos/240p.mp4'
+import duck_360p from './videos/360p.mp4'
+import duck_480p from './videos/480p.mp4'
+import duck_720p from './videos/original.mp4'
 
 const source: Source = [
     // 'https://cdn.discordapp.com/attachments/837976157609656323/842785794485780520/Shrek_1.mp4',
     // 'https://cdn.discordapp.com/attachments/876184324133752874/944696426544373790/nima_1384_bzan_20220219_222030_0.mp4',
-    // blue,
-    // green,
-    // red,
     {
-        label: 'Blue',
-        url: blue,
+        label: '144p',
+        url: duck_144p,
     },
     {
-        label: 'Green',
-        url: green,
+        label: '240p',
+        url: duck_240p,
     },
     {
-        label: 'Red',
-        url: red,
+        label: '360p',
+        url: duck_360p,
+    },
+    {
+        label: '480p',
+        url: duck_480p,
+    },
+    {
+        label: '720p',
+        url: duck_720p,
     },
 ]
 
