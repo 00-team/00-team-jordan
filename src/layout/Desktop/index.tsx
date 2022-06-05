9f81a48c8681c57a34a13304ecbaf917f6a7636c
007 <dr007cc@gmail.com>
2022-06-05 17:06:15 +0430

    hiding the desktop controls

--- src/layout/Desktop/index.tsx
+++ src/layout/Desktop/index.tsx
@@ -5,17 +5,53 @@ import Controls from './Controls'
 
 import './style/index.scss'
 
-interface DesktopProps {}
+interface DesktopState {
+    hide: boolean
+    clock?: NodeJS.Timeout
+}
 
-interface DesktopState {}
+var LastActivity = 0
+const now = () => new Date().getTime()
 
-class Desktop extends BaseComponent<DesktopProps, DesktopState> {
-    override state: DesktopState = {}
+class Desktop extends BaseComponent<{}, DesktopState> {
+    override state: DesktopState = {
+        hide: false,
+    }
+
+    private UpdateHide(hide: boolean = true) {
+        if (this.video.paused && document.fullscreenElement !== this.vito)
+            hide = false
+
+        if (hide) this.vito.style.cursor = 'none'
+        else this.vito.style.cursor = ''
+
+        this.setState({ hide: hide })
+    }
+
+    private UpdateActivity = this.UpdateActivityPR.bind(this)
+    private UpdateActivityPR() {
+        LastActivity = now()
+        this.UpdateHide(false)
+    }
+
+    override componentDidMount() {
+        document.addEventListener('mousemove', this.UpdateActivity)
+
+        let clock = setInterval(() => {
+            if (now() - LastActivity > 3_000) this.UpdateHide()
+        }, 500)
+        this.setState({ clock: clock })
+    }
+
+    override componentWillUnmount() {
+        document.removeEventListener('mousemove', this.UpdateActivity)
+        if (this.state.clock) clearInterval(this.state.clock)
+    }
 
     override render(): ReactElement {
         return (
             <div className='desktop-layout'>
-                <Controls />
+                {!this.state.hide && <Controls />}
                 <div
                     className='toggle'
                     onClick={() => {
